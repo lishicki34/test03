fe546eebae6f4e80fb0575ec89b53e585b1139ee
Vladislav Stepanov <lishicki34@mail.ru>
2025-02-03 02:00:43 +0300

    0302

---
+++ script.js
@@ -0,0 +1,7 @@
+// function checkOrder(s) {
+//   const indexR = s.indexOf('r');
+//   const indexM = s.indexOf('m')
+
+//   return indexR < indexM ? "yes" : 'no';
+// } 
+// console.log(checkOrder("smr"));
